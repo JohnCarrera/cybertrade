901d1e884943b8877b3bf71694a8fe6227af0dc1
JohnCarrera <johnrcarrera@gmail.com>
2022-11-02 18:21:14 -0400

    added assets table with styling'' ''

--- react-app/src/components/dashboard/AssetPanel.js
+++ react-app/src/components/dashboard/AssetPanel.js
@@ -2,50 +2,82 @@ import React from 'react';
 import './assetPanel.css';
 
 export default function AssetPanel({ assets, assetVal, stocks }) {
+
+    function convertPad(num) {
+        if (num.toString().includes('.')) {
+            let nums = num.toString().split('.')
+            let n1 = nums[0];
+            let n2 = nums[1];
+            n2 = n2.padEnd(2, '0')
+            return `${n1}.${n2}`
+        }
+        return String(num) + '.00'
+    }
+
     return (
         <div className='ap-main'>
-            <div className='ap-header-row'>
-                <div className='ap-header-cell'>
-                    Name
-                </div>
-                <div className='ap-header-cell'>
-                    Symbol
-                </div>
-                <div className='ap-header-cell'>
-                    Type
-                </div>
-                <div className='ap-header-cell'>
-                    Qty
-                </div>
-                <div className='ap-header-cell'>
-                    PPS
-                </div>
-                <div className='ap-header-cell'>
-                    Orig. Value
-                </div>
-                <div className='ap-header-cell'>
-                    Asset Name
-                </div>
-                <div className='ap-header-cell'>
-                    Current Price
-                </div>
-                <div className='ap-header-cell'>
-                    Current Value
-                </div>
-            </div>
-            {Object.values(assets).map(asset => ( asset.symbol !== '_CASH' &&
-                <div className='ap-row'>
-                    <div className='ap-row-cell ap-font'>
-                        {stocks[asset.symbol].name}
-                    </div>
-                    <div className='ap-row-cell ap-font'>
-                        {asset.symbol}
-                    </div>
-                    <div className='ap-row-cell ap-font'>
-                        {asset.type}
-                    </div>
-                </div>
-            ))}
-        </div>
+            <table className='ap-table-main'>
+                <thead className='ap-thead'>
+                    <tr>
+                        <th className='ap-th'>
+                            Name
+                        </th>
+                        <th className='ap-th'>
+                            Symbol
+                        </th>
+                        <th className='ap-th'>
+                            Qty
+                        </th>
+                        <th className='ap-th'>
+                            PPS
+                        </th>
+                        <th className='ap-th'>
+                            Orig. Value
+                        </th>
+                        <th className='ap-th'>
+                            Curr. PPS
+                        </th>
+                        <th className='ap-th'>
+                            Curr. Value
+                        </th>
+                        <th className='ap-th'>
+                            P/L ($/%)
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.values(assets).map(asset => (asset.symbol !== '_CASH' &&
+                        <tr>
+                            <td className='ap-td'>
+                                {stocks[asset.symbol].name}
+                            </td>
+                            <td className='ap-td'>
+                                {asset.symbol}
+                            </td>
+                            <td className='ap-td'>
+                                {asset.quantity}
+                            </td>
+                            <td className='ap-td'>
+                                {asset.value}
+                            </td>
+                            <td className='ap-td'>
+                                {convertPad(asset.value * asset.quantity)}
+                            </td>
+                            <td className='ap-td'>
+                                {asset.value}
+                            </td>
+                            <td className='ap-td'>
+                                {convertPad(asset.value * asset.quantity)}
+                            </td>
+                            <td className='ap-td'>
+                                {convertPad(asset.value * asset.quantity - asset.value * asset.quantity)}
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+
+            </table>
+        </div >
     )
 }
